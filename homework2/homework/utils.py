fbf988b6ae361445390a1336d70f66b434a362be
jackgopack4 <jlpeterson@utexas.edu>
2021-10-03 17:56:57 +0000

    hw2 add utils from hw1 solution

--- homework2/homework/utils.py
+++ homework2/homework/utils.py
@@ -14,20 +14,29 @@ class SuperTuxDataset(Dataset):
         Your code here
         Hint: Use your solution (or the master solution) to HW1
         """
-        raise NotImplementedError('SuperTuxDataset.__init__')
-
+        import csv
+        from os import path
+        self.data = []
+        to_tensor = transforms.ToTensor()
+        with open(path.join(dataset_path, 'labels.csv'), newline='') as f:
+            reader = csv.reader(f)
+            for fname, label, _ in reader:
+                if label in LABEL_NAMES:
+                    image = Image.open(path.join(dataset_path, fname))
+                    label_id = LABEL_NAMES.index(label)
+                    self.data.append((to_tensor(image), label_id))
     def __len__(self):
         """
         Your code here
         """
-        raise NotImplementedError('SuperTuxDataset.__len__')
+        return len(self.data)
 
     def __getitem__(self, idx):
         """
         Your code here
         return a tuple: img, label
         """
-        raise NotImplementedError('SuperTuxDataset.__getitem__')
+        return self.data[idx]
 
 
 def load_data(dataset_path, num_workers=0, batch_size=128):
@@ -38,3 +47,37 @@ def load_data(dataset_path, num_workers=0, batch_size=128):
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
+
+if __name__ == "__main__":
+    """
+    Optional:
+    This code down here allows you to measure how fast your data loader is.
+    The master solution takes about 2s to __init__ on a desktop CPU, and 0.2 / epoch.
+    """
+    from time import time
+    import argparse
+    parser = argparse.ArgumentParser("Loads the entire dataset once")
+    parser.add_argument('dataset')
+    parser.add_argument('-d', '--use_data_loader', action='store_true')
+    parser.add_argument('-p', '--plot', action='store_true')
+    parser.add_argument('-n', '--n_epoch', type=int, default=1)
+    args = parser.parse_args()
+
+    if args.use_data_loader:
+        L = load_data
+    else:
+        L = SuperTuxDataset
+
+    t0 = time()
+    data = L(args.dataset)
+
+    epoch_t = [time() - t0]
+    for i in range(args.n_epoch):
+        list(data)
+        epoch_t.append(time() - t0)
+    if args.plot:
+        import pylab as plt
+        plt.plot(epoch_t)
+        plt.show()
+    else:
+        print('Timing:', epoch_t)
