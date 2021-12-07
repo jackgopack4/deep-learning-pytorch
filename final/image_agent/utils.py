a333ea4582f7841dffcad6b07414e19931d14d9f
jackgopack4 <jlpeterson@utexas.edu>
2021-12-07 15:49:05 +0000

    remove print stmt from utils and train

--- final/image_agent/utils.py
+++ final/image_agent/utils.py
@@ -22,7 +22,6 @@ def puck_in_frame(instance):
 def load_recording(recording):
     while True:
         try:
-            #print('loading recording')
             yield load(recording)
         except EOFError:
             break
@@ -35,38 +34,25 @@ class SuperTuxDataset(Dataset):
         self.pucks = []
         self.locs = []
         self.data = []
-        j = 0;
         with(open(dataset_path, 'rb')) as f:
             for d in load_recording(f):
-                #print('called load_recording')
-                j+=1
-                #print('j=',j)
-                #if(j > 127): break
                 team1_images = d.get('team1_images')
                 team1_projectile = d.get('team1_projectile')
                 team2_images = d.get('team2_images')
                 team2_projectile = d.get('team2_projectile')
                 ball_loc = d.get('soccer_state').get('ball').get('location')
-                #print('team1_images length =',len(team1_images))
                 for i in range(0,len(team1_images)):
                     self.images.append(team1_images[i])
-                    #print(data'team1_image=',img)
                     proj = d.get('team1_state')[i].get('camera').get('projection')
                     view = d.get('team1_state')[i].get('camera').get('view')
                     self.pucks.append(float(d.get('team1_projectile')[i]))
                     self.locs.append(to_image(ball_loc,proj,view))
-                    #self.labels.append((in_frame,coord[0],coord[1]))
-                    #self.data.append((img, in_frame,coord))
                     self.images.append(team2_images[i])
                     proj = d.get('team2_state')[i].get('camera').get('projection')
                     view = d.get('team2_state')[i].get('camera').get('view')
                     self.pucks.append(float(d.get('team2_projectile')[i]))
                     self.locs.append(to_image(ball_loc,proj,view))
-                    #self.labels.append((in_frame,coord[0],coord[1]))
-                    #self.data.append((img, in_frame,coord))
-        #print(self.data)
         self.transform = transform
-        #print(self.transform)
 
     def __len__(self):
         return len(self.images)
@@ -77,11 +63,8 @@ class SuperTuxDataset(Dataset):
         loc = self.locs[idx]
         if self.transform:
             img = Image.fromarray(img)
-            #print(img)
             img = self.transform(img)
         data = (img[0], torch.tensor(puck), torch.tensor(loc))
-        #print('returning data at idx',idx)
-        #print(data)
         return data
 
 def load_data(dataset_path=DATASET_PATH, transform=dense_transforms.ToTensor(), num_workers=0, batch_size=128):
